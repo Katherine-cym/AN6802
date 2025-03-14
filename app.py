25a4e2bea26020938ea1c8c1a7007a23467487b5
Katherine-cym <yimeng_cao@outlook.com>
2025-03-14 11:57:22 +0000

    3

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def main():
     name = request.form.get("name")    
     return(render_template("main.html",name=name))
 
+@app.route("/foodexp",methods=["GET","POST"])
+def foodexp():   
+    return(render_template("foodexp.html"))
+
 @app.route("/ethical_test",methods=["GET","POST"])
 def ethical_test():   
     return(render_template("ethical_test.html"))
@@ -21,6 +25,12 @@ def test_result():
     elif answer == "true":
         return(render_template("fail.html"))
 '''
+@app.route("/foodexp_pred",methods=["POST","GET"])
+def foodexp_pred():
+    q = float(request.form.get("q"))
+    return(render_template("foodexp_pred.html", r = (q*0.4851+ 147.475)))
+
+
 @app.route("/test_result",methods=["POST","GET"])
 def test_result():
     answer=request.form.get("answer")
